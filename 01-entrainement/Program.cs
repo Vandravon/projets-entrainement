a93485678f8c8e1b5b1d713df100a6025d6e7c5a
Vandravon <vincent.drvdpt@gmail.com>
2023-01-01 18:13:12 +0100

    Adding Exercice 01

---
+++ 01-entrainement/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Le but était de m'entraîner sur différentes choses: array, boucle while, boucle for
+// J'ai aussi voulu utiliser la même variable i à 2 endroits différents, pour me servir du concept du scope
+
+int[] array = new int[3];
+
+{
+ int i = 0;
+ int answer = 0;
+ while (i < array.Length)
+ {
+  Console.WriteLine($"Choisissez un nombre entre 1 et 50 ({i}):");
+  answer = Convert.ToInt32(Console.ReadLine());
+
+  if (i <= 50 || i >= 1)
+  {
+   Console.WriteLine("Noté!");
+   array[i] = answer;
+   i++;
+  }
+  else
+  {
+   Console.WriteLine("Vous devez sélectionner un nombre entre 1 et 50!!");
+  }
+ }
+
+}
+
+Console.WriteLine("Vous avez sélectionné:");
+
+{
+ for (int i = 0; i < array.Length; i++)
+ {
+  Console.WriteLine(array[i]);
+ }
+}
